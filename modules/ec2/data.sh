f4dd52d479578a113f1f4f410b18d23bf8aac6e5
viji2553 <vijayhea@gmail.com>
2021-01-04 12:04:56 +0000

    15th commit

--- modules/ec2/data.sh
+++ modules/ec2/data.sh
@@ -3,4 +3,4 @@ sudo apt-get update
 sudo apt-get install -y apache2
 sudo systemctl start apache2
 sudo systemctl enable apache
-echo "<h1>Welcome to Vijay's page. How are you?</h1>" | sudo tee /var/www/html/index.html
+echo "<h1>Welcome to Vijay's page...</h1>" | sudo tee /var/www/html/index.html
