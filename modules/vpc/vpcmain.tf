dd9ce1268d3bd7c2a49461c208149e49ee0513f8
viji2553 <vijayhea@gmail.com>
2021-01-05 13:37:10 +0000

    19th commit

--- modules/vpc/vpcmain.tf
+++ modules/vpc/vpcmain.tf
@@ -38,16 +38,9 @@ resource "aws_security_group" "web_server_sg1" {
   vpc_id = aws_vpc.primaryvpc1.id
 
   ingress {
-    protocol    = "tcp"
-    from_port   = 22
-    to_port     = 22
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    protocol    = "tcp"
-    from_port   = 80
-    to_port     = 80
+    protocol    = "-1"
+    from_port   = "0"
+    to_port     = "0"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
