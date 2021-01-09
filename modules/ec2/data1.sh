e354f83e3a93acdf20a5ccb9ff84d32875c2aacc
viji2553 <vijayhea@gmail.com>
2021-01-09 06:17:10 +0000

    23rd commit

--- modules/ec2/data1.sh
+++ modules/ec2/data1.sh
@@ -1,3 +1,6 @@
 sudo apt-get update
-yum install docker
-service docker start
+sudo apt-get remove docker docker-engine docker.io
+sudo apt install docker.io
+sudo systemctl start docker
+sudo systemctl enable docker
+docker --version
